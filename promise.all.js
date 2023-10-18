88259100034481670718e8bbc8c558f6bd1371c0
Nabina aryal <106301164+nabinaaryal-2002@users.noreply.github.com>
2023-10-18 10:26:58 +0545

    promise

--- promise.all.js
+++ promise.all.js
@@ -8,7 +8,7 @@ let stocks = {
 let is_shop_open = true;
 // let is_shop_open = false;
 
-function order(time, work) {
+const order = (time, work) => {
   return new Promise((resolve, reject) => {
     if (is_shop_open) {
       setTimeout(() => {
@@ -18,20 +18,35 @@ function order(time, work) {
       reject("our shop is closed");
     }
   });
-}
+};
 
-const steps = [
-  () => order(2000, () => `${stocks.Fruits[0]} was selected`),
-  () => order(0, () => "production has started"),
-  () => order(2000, () => "the fruit was chopped"),
-  () => order(1000, () => `${stocks.liquid[0]} and ${stocks.liquid[1]} was selected`),
-  () => order(1000, () => "start the machine"),
-  () => order(2000, () => `ice cream placed on ${stocks.holder[0]}`),
-  () => order(3000, () => `${stocks.toppings[0]} was selected`),
-  () => order(1000, () => "Ice cream was served"),
-];
-
-Promise.all(steps.map(step => step()))
-  .then(() => console.log("All steps completed successfully"))
-  .catch(error => console.log(error))
-  .finally(() => console.log("Day ended, shop is closed"));
+Promise.all([
+  order(2000, () => console.log(`${stocks.Fruits[0]} was selected`)),
+  order(0, () => console.log("production has started")),
+  order(2000, () => console.log("the fruit was chopped")),
+  order(1000, () =>
+    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was selected`)
+  ),
+])
+  .then(() => {
+    console.log("Starting the machine");
+    return order(1000, () => "start the machine");
+  })
+  .then(() => {
+    console.log(`Ice cream placed on ${stocks.holder[0]}`);
+    return order(2000, () => "Ice cream placed on the holder");
+  })
+  .then(() => {
+    console.log(`${stocks.toppings[0]} was selected`);
+    return order(3000, () => "Toppings were selected");
+  })
+  .then(() => {
+    console.log("Ice cream was served");
+    return order(1000, () => "Ice cream was served");
+  })
+  .catch(() => {
+    console.log("Customer left");
+  })
+  .finally(() => {
+    console.log("Day ended, shop is closed");
+  });
